aa7271aad63bec1dc6d140265ba279915b5f6232
Бондаренко Евгений <bondev6@gmail.com>
2018-02-02 14:04:32 +0300

    test

--- Gateway.php
+++ Gateway.php
@@ -87,6 +87,7 @@ class Gateway implements GatewayInterface
      */
     public function execute($request, $catchReply = false)
     {
+        /*test*/
         $context = new Context($this, $request, $this->stack);
 
         array_push($this->stack, $context);
